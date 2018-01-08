2ae946f3a1ffc21e59f851db33ba7dc1cc3b0b9a
shreya <shreya.bs12@gmail.com>
2018-01-08 16:31:22 +0530

    Updating progress bar to reflect the current battery status.

--- app/src/main/java/com/shreya/batterystatusindicator/BatteryIndicatorActivity.kt
+++ app/src/main/java/com/shreya/batterystatusindicator/BatteryIndicatorActivity.kt
@@ -3,6 +3,9 @@ package com.shreya.batterystatusindicator
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ProgressBar
@@ -22,31 +25,36 @@ class BatteryIndicatorActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-
+        //Update the progress bar
+        updateBatteryLevel()
     }
 
-    fun updateBatteryLevel(){
-        var batteryBroadcastReceiver : BroadcastReceiver = object : BroadcastReceiver() {
+
+    private fun updateBatteryLevel(){
+        val batteryBroadcastReceiver : BroadcastReceiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context?, intent: Intent?) {
-                context?.unregisterReceiver(this)
+                context?.unregisterReceiver(this) //Continuing to monitor the status
+                                                     // can lead to battery drain
 
-                val rawlevel = intent?.getIntExtra("level", -1)
-                val scale = intent?.getIntExtra("scale", -1)
+                val batteryLevel = intent?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) as Int
+                val scale = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
 
-                val level = -1
+                var level = -1
 
-                /*if(rawlevel?. >= 0 && scale > 0)
+                if(batteryLevel >= 0 && scale > 0){
+                    level = (batteryLevel * 100) / scale
 
-                int rawlevel = intent.getIntExtra("level", -1);
-                int scale = intent.getIntExtra("scale", -1);
-                int level = -1;
-                if (rawlevel >= 0 && scale > 0) {
-                    level = (rawlevel * 100) / scale;*/
+                }
 
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    mBatteryStatusBar?.setProgress(level, false)
+                }else{
+                    mBatteryStatusBar?.progress = level
+                }
             }
-
         }
 
-
+        val batteryIntentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+        registerReceiver(batteryBroadcastReceiver, batteryIntentFilter)
     }
 }
